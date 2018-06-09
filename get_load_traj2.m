e486726d4425b5ed375407c0aa658e243b4260ad
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-06-08 17:43:57 -0700

    Hovering Test

---
+++ get_load_traj2.m
@@ -0,0 +1,11 @@
+function load_traj = get_load_traj2(t)
+%% Return to the origin
+load_traj.xL = [0;0;0];
+load_traj.dxL = [0;0;0];
+load_traj.d2xL = [0;0;0];
+load_traj.d3xL = [0;0;0];
+load_traj.d4xL = [0;0;0];
+load_traj.d5xL = [0;0;0];
+load_traj.d6xL = [0;0;0];
+
+end
